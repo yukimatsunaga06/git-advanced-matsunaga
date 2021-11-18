2c10ba1a79ee6ad8a69a3c32fea872a5ce8e9e07
yukimatsunaga <yuki.matsunaga@rakus-partners.co.jp>
2021-11-18 10:31:06 +0900

    first commit

---
+++ src/main/java/com/example/main/Car.java
@@ -0,0 +1,45 @@
+package com.example.main;
+/**
+ * 車のドメイン
+ * @author yukimatsunaga
+ *
+ */
+
+public class Car {
+
+	private int speed;
+	private String name;
+	
+	
+	
+	public Car() {
+		
+	}
+	
+	
+	public Car(int speed, String name) {
+		super();
+		this.speed = speed;
+		this.name = name;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", name=" + name + "]";
+	}
+	
+}
